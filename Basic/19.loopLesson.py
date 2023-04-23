fc70ebbc1abbb7d11514bd434b1f8c5e30a77c29
Khine <khinezar70122107@gmail.com>
2023-04-22 15:26:35 -1200

    while loop

---
+++ Basic/19.loopLesson.py
@@ -0,0 +1,22 @@
+#loop ထပ်ခါထပ်ခါအလုပ်လုပ်ခြင်း
+#for loop
+#while loop
+#syntax
+#while condition:True
+    #instruction
+    #increment/decrement
+
+
+a = 1
+b =4
+while a < b:
+    print("Hello",a)
+    a+=1
+    # a = a - 1
+
+#infinite
+#Hello
+#Hello
+#Hello
+#Hello
+
